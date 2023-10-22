89211dbb6b9ad5e1d9ac1417f559fcd9ae413ab4
gatnalysing <137360061+gatnalysing@users.noreply.github.com>
2023-10-22 23:08:44 +0000

    Update opta.cc
    1. "USER" button needs to be held for 10 seconds to avoid accidental initiation.
    2. Once button has been held for 10 seconds LED starts blinking.
    3. Factory reset power cycling sequence commences immediately upon button release...
    4.... unless button is held for 20 seconds. Then the sequence will  cancel.
    5. Once power cycling sequence starts, it can be calcelled by pressing the button briefly once

--- opta.cc
+++ opta.cc
@@ -1,42 +1,82 @@
-int button_state = 0;  // Breyta til að lesa stöðu á rofa
+const int buttonPin = BTN_USER; // "USER" button
+const int relayPin = D0;
+const int ledPin = LED_D0;
+
+bool isButtonHeld = false;
+unsigned long buttonPressedTime = 0;
+bool flashing = false;
 
 void setup() {
-  pinMode(D0, OUTPUT); // Arduino opta Relay 1
-  pinMode(LED_D0, OUTPUT);  //Arduino opta LED 0
+  pinMode(relayPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP); // BTN_USER is active LOW
+  digitalWrite(ledPin, HIGH);
   delay(1000);
 }
 
-void state_change(){
-  digitalWrite(LED_D0,LOW);
-  digitalWrite(D0, LOW);
-  delay(5000);
-  digitalWrite(LED_D0, HIGH);
-  digitalWrite(D0, HIGH);
-  delay(5000);
+bool state_change() {
+  digitalWrite(relayPin, LOW);
+  if (flashingDelay(5000)) return true;
+  digitalWrite(relayPin, HIGH);
+  if (flashingDelay(5000)) return true;
+  return false;
+}
+
+bool flashingDelay(unsigned long duration) {
+  unsigned long startTime = millis();
+  while (millis() - startTime < duration) {
+    if (flashing) {
+      if ((millis() / 250) % 2 == 0) {
+        digitalWrite(ledPin, HIGH);
+      } else {
+        digitalWrite(ledPin, LOW);
+      }
+    }
+    if (digitalRead(buttonPin) == LOW) { // Button is pressed
+      return true; // Interrupted
+    }
+    delay(1); // Short delay to avoid blocking
+  }
+  return false; // Completed without interruption
 }
 
 void loop() {
-  button_state = digitalRead(BTN_USER);  // BTN_USER er innbyggði rofinn
-  if (button_state == LOW){
-    delay(1500);
-    state_change();
-    state_change();
-    state_change();
-  // partur 2
-    state_change();
-    delay(5000);
-  // partur 3
-    state_change();
-    digitalWrite(LED_D0,LOW);
-    digitalWrite(D0, LOW);
-    delay(5000);
-    digitalWrite(LED_D0,HIGH);
-    digitalWrite(D0, HIGH);
+  int button_state = digitalRead(buttonPin);
+  if (button_state == LOW) { // Button is pressed
+    if (!isButtonHeld) {
+      buttonPressedTime = millis();
+      isButtonHeld = true;
+    }
+    unsigned long currentTime = millis();
+    if (currentTime - buttonPressedTime >= 10000) {
+      flashing = true; // Start flashing when button is held for 10 seconds
+    }
+    if (currentTime - buttonPressedTime > 20000) {
+      isButtonHeld = false; // Reset if held for more than 20 seconds
+      flashing = false; // Stop flashing
+      digitalWrite(ledPin, HIGH);
+    }
+  } else { // Button is not pressed
+    if (isButtonHeld) {
+      unsigned long buttonReleasedTime = millis();
+      if (buttonReleasedTime - buttonPressedTime >= 10000 && buttonReleasedTime - buttonPressedTime <= 20000) {
+        if (state_change()) return;
+        if (state_change()) return;
+        if (state_change()) return;
+        if (state_change()) return;
+        flashingDelay(5000);
+        state_change();
+        digitalWrite(relayPin, LOW);
+        delay(5000);
+        digitalWrite(relayPin, HIGH);
+        flashing = false; // Stop flashing after power cycling is completed
+      }
+      isButtonHeld = false; // Reset button hold state
+    }
+    if (!flashing) {
+      digitalWrite(ledPin, HIGH);
+      digitalWrite(relayPin, HIGH);
+      delay(1000);
+    }
   }
-  else {  
-    digitalWrite(LED_D0,HIGH);
-    digitalWrite(D0, HIGH);
-    delay(1000);
-  }
-    
 }
